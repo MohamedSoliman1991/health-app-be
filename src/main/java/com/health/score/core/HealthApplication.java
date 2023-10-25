d06f871b71613dfe18d6e1280e5bbb0b2e76ef0a
Mohd.Soliman <mohf.soliman@dpworld.com>
2023-10-25 09:36:38 +0400

    init

---
+++ src/main/java/com/health/score/core/HealthApplication.java
@@ -0,0 +1,13 @@
+package com.health.score.core;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HealthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HealthApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/health/score/core/SpringBootHerokuDevopsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.health.score.core;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootHerokuDevopsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
